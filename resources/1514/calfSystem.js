2f24a8aa352072e315e1af0ff5116ea3cdbf96fc
atompkins <atompkins@gmail.com>
2016-06-13 22:21:00 +0100

    Arena

--- resources/1514/calfSystem.js
+++ resources/1514/calfSystem.js
@@ -11763,7 +11763,7 @@ FSH.arena = { // jQuery
 		var myRows = theTables.children('tbody').children('tr');
 		myRows.each(FSH.arena.orderData);
 
-		var sheets = document.styleSheets;
+		var sheets = document.styleSheets[3].cssRules;
 		console.log('sheets', sheets);
 
 		FSH.ajax.setForage('fsh_arena', FSH.arena.opts);
