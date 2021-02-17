b03962d1d4df3a435c2d1a5991600a4baf4e21dc
atompkins <atompkins@gmail.com>
2021-02-17 14:59:12 +0000

    DEV update one-by-one to use async/await

--- src/modules/trade/oneByOne.js
+++ src/modules/trade/oneByOne.js
@@ -6,32 +6,25 @@ import onclick from '../common/onclick';
 import querySelector from '../common/querySelector';
 import querySelectorArray from '../common/querySelectorArray';
 
-function sendThem(prm, options) {
-  return prm.then((data) => {
-    // eslint-disable-next-line no-console
-    console.log('promise data', data);
-    if (data === null || data.s) {
-      return daSendItems(options[0], options[1]);
-    }
-    return data;
-  });
+async function sendThem(prm, options) {
+  const data = await prm;
+  // eslint-disable-next-line no-console
+  console.log('promise data', data);
+  if (data === null || data.s) { return daSendItems(options[0], options[1]); }
+  return data;
 }
 
-function onBtnClick() {
-  const user = querySelector(
-    'form[name="sendItemForm"] [name="target_username"]',
-  );
-  const items = querySelectorArray('[name="sendItemList[]"]:checked');
-  items.map((el) => [user.value, [el.value]])
-    .reduce(sendThem, Promise.resolve(null))
-    // eslint-disable-next-line no-console
-    .then((finalResult) => { console.log('finalResult', finalResult); });
+async function onBtnClick() {
+  const user = querySelector('form[name="sendItemForm"] [name="target_username"]');
+  const items = querySelectorArray('[name="sendItemList[]"]:checked')
+    .map((el) => [user.value, [el.value]]);
+  const finalResult = await items.reduce(sendThem, Promise.resolve(null));
+  // eslint-disable-next-line no-console
+  console.log('finalResult', finalResult);
 }
 
 export default function oneByOne() {
-  const sendItemBtn = querySelector(
-    'form[name="sendItemForm"] input[value="Send"]',
-  );
+  const sendItemBtn = querySelector('form[name="sendItemForm"] input[value="Send"]');
   const myBtn = createButton({
     className: 'fshBl',
     id: 'oneByOneBtn',
