b6e497f52eba2a73904b436c9980d3bdc4d2ed78
atompkins <atompkins@gmail.com>
2021-03-23 10:51:03 +0000

    Add versioning to APP API call

--- src/modules/app/callApp.js
+++ src/modules/app/callApp.js
@@ -4,7 +4,7 @@ import retryAjax from '../ajax/retryAjax';
 export default function callApp(data) {
   return retryAjax({
     url: 'app.php',
-    data: extend(data, { browser: 1 }),
+    data: extend(data, { browser: 1, v: '0.8.7' }),
     dataType: 'json',
   });
 }
