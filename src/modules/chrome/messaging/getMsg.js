bc7f1c6fc5df88e6304053a1eebbd6837d720b58
atompkins <atompkins@gmail.com>
2021-04-01 15:39:06 +0100

    update message length

--- src/modules/chrome/messaging/getMsg.js
+++ src/modules/chrome/messaging/getMsg.js
@@ -5,7 +5,7 @@ let dialogMsg;
 export default function getMsg() {
   if (!dialogMsg) {
     dialogMsg = getElementById('quickMsgDialog_msg');
-    dialogMsg.maxLength = 256;
+    dialogMsg.maxLength = 512;
   }
   return dialogMsg;
 }
