f2e831c76c07f163493c0bd0a100a154d4910dd2
atompkins <atompkins@gmail.com>
2021-07-10 02:02:14 +0100

    sonar nuclear option

--- scripts/esserve.js
+++ scripts/esserve.js
@@ -1,27 +1,43 @@
 'use strict';
 
 const { port: calfPort } = require('./config.json');
+const cleanTarget = require('./cleanTarget');
 const esbuild = require('esbuild');
 const process = require('process');
-const proxy = require('./esproxy');
 const sveltePlugin = require('esbuild-svelte');
 const { calfVer, core } = require('./getVersion');
+const {
+  readFileSync,
+  writeFileSync,
+} = require('fs');
 
-// Start esbuild's server on a random local port
+const dist = 'dist/';
+const fshPath = 'Releases/watch';
+const calfPath = `resources/watch/${core}`;
+
+cleanTarget(`${dist}${fshPath}`);
+
+const fsh = readFileSync('src/fallenswordhelper.user.js', 'utf8')
+  .replaceAll('_VER', `${core}a`)
+  .replace('_DLURL', `http://localhost:${calfPort}/${dist}${fshPath}/fallenswordhelper.user.js`)
+  .replace('_CALFJS', `http://localhost:${calfPort}/${dist}${calfPath}/calfSystem.js`);
+writeFileSync(`${dist}${fshPath}/fallenswordhelper.user.js`, fsh);
+
 esbuild.serve({
+  port: Number(calfPort),
   servedir: process.cwd(),
 }, {
   bundle: true,
   chunkNames: `${calfVer}/[name]-[hash]`,
   define: {
-    defineDataTablesPath: `"https://localhost:${calfPort}/src/styles/dataTables.css"`,
-    defineCalfPath: `"https://localhost:${calfPort}/dist/resources/watch/${core}/calfSystem.css"`,
+    defineDataTablesPath: `"http://localhost:${calfPort}/src/styles/dataTables.css"`,
+    defineCalfPath: `"http://localhost:${calfPort}/${dist}${calfPath}/calfSystem.css"`,
     defineCalfVer: `"${calfVer}"`,
     defineUserIsDev: true,
   },
   entryPoints: ['src/calfSystem.js'],
   format: 'esm',
-  outdir: `dist/resources/watch/${core}`,
+  outdir: `${dist}${calfPath}`,
   plugins: [sveltePlugin()],
   splitting: true,
-}).then(proxy);
+});
