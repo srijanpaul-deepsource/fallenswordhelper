db804b89a4545e6c6e5ee3f74b695ad1f25f354f
Adrian Tompkins <atompkins@users.noreply.github.com>
2021-10-25 00:29:12 +0100

    change-labels-fix (#271)

--- src/modules/logs/playerLogWidgets/changeLabels.js
+++ src/modules/logs/playerLogWidgets/changeLabels.js
@@ -1,8 +1,10 @@
+import getCustomUrlParameter from '../../system/getCustomUrlParameter';
 import querySelectorArray from '../../common/querySelectorArray';
 import setText from '../../dom/setText';
 
 export default function changeLabels(logTable) {
   querySelectorArray('a[href*="=trade&"]', logTable)
+    .filter((a) => !getCustomUrlParameter(a.href, 'subcmd'))
     .forEach((a) => setText('Trade', a));
   querySelectorArray('a[href*="=createsecure&"]', logTable)
     .forEach((a) => setText('ST', a));
