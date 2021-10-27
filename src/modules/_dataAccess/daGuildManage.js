015ca9fae0b05772d5a99c7cc58db8a17ea0ea9d
Adrian Tompkins <atompkins@users.noreply.github.com>
2021-10-27 11:23:56 +0100

    fallback-guild-manage (#274)
    * fallback-guild-manage
    
    * another way

--- src/modules/_dataAccess/daGuildManage.js
+++ src/modules/_dataAccess/daGuildManage.js
@@ -3,5 +3,9 @@ import appGuildManage from '../app/guild/manage';
 import guildManage from './fallbacks/guildManage';
 
 export default function daGuildManage() {
+  const bugged = 1;
+  if (bugged) {
+    return guildManage();
+  }
   return $dataAccess(appGuildManage, guildManage);
 }
