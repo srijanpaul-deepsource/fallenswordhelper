0dd305e2d0a878678ff7033d4c6ef233394c359b
Adrian Tompkins <atompkins@users.noreply.github.com>
2021-12-05 16:41:06 +0000

    pref destroy prompts #293 (#295)

--- src/modules/profile/dropitems/interceptDestroy.js
+++ src/modules/profile/dropitems/interceptDestroy.js
@@ -1,6 +1,7 @@
 import chunk from '../../common/chunk';
 import daDropItems from '../../_dataAccess/daDropItems';
 import errorDialog from '../../common/errorDialog';
+import eventHandler5 from '../../common/eventHandler5';
 import getCheckboxesArray from '../../guild/inventory/storeitems/getCheckboxesArray';
 import getCheckedItems from './getCheckedItems';
 import getValue from '../../system/getValue';
@@ -8,50 +9,80 @@ import insertHtmlBeforeEnd from '../../common/insertHtmlBeforeEnd';
 import on from '../../common/on';
 import querySelector from '../../common/querySelector';
 import removeRow from './removeRow';
+import selfIdIs from '../../common/selfIdIs';
 import sendEvent from '../../analytics/sendEvent';
 import setValue from '../../system/setValue';
 import { simpleCheckboxHtml } from '../../settings/simpleCheckbox';
 
-function check(mode) {
+function scopedCheck(mode) {
   getCheckboxesArray().forEach((ctx) => { ctx.checked = Boolean(mode); });
 }
 
 const prefAjaxifyDestroy = 'ajaxifyDestroy';
+const prefDisableDestroyPrompts = 'disableDestroyPrompts';
 let ajaxifyDestroy;
+let disableDestroyPrompts;
+let hcsConfirmDestroy;
 
-const destroyChunk = (itemsAry) => {
-  daDropItems(itemsAry.map((i) => i.value))
-    .then(errorDialog)
-    .then((json) => {
-      if (!json.s) { return; }
-      itemsAry.forEach(removeRow);
-    });
-};
+async function destroyChunk(itemsAry) {
+  const json = await daDropItems(itemsAry.map((i) => i.value));
+  errorDialog(json);
+  if (!json.s) { return; }
+  itemsAry.forEach(removeRow);
+}
 
-const checkItems = (e) => {
+function submitHandler(e) {
   if (!e.returnValue || !ajaxifyDestroy) { return; }
   e.preventDefault();
   chunk(30, getCheckedItems()).forEach(destroyChunk);
   sendEvent('dropitems', 'Destroy by AJAX');
-};
+}
 
-function changePref() {
+function fshConfirmDestroy() {
+  sendEvent('dropitems', 'Destroy without prompts');
+  return true;
+}
+
+function setDestroyPrompt() {
+  window.confirmDestroy = disableDestroyPrompts ? fshConfirmDestroy : hcsConfirmDestroy;
+}
+
+function initDestroyPrompt() {
+  disableDestroyPrompts = getValue(prefDisableDestroyPrompts);
+  hcsConfirmDestroy = window.confirmDestroy;
+  setDestroyPrompt();
+}
+
+function handleAjaxifyPref() {
   ajaxifyDestroy = !ajaxifyDestroy;
   setValue(prefAjaxifyDestroy, ajaxifyDestroy);
 }
 
-const injectPref = () => {
+function handleDestroyPref() {
+  disableDestroyPrompts = !disableDestroyPrompts;
+  setValue(prefDisableDestroyPrompts, disableDestroyPrompts);
+  setDestroyPrompt();
+}
+
+const changePref = () => eventHandler5([
+  [selfIdIs(prefAjaxifyDestroy), handleAjaxifyPref],
+  [selfIdIs(prefDisableDestroyPrompts), handleDestroyPref],
+]);
+
+function injectPrefs() {
   const submitBtn = querySelector('input[type="submit"]');
   insertHtmlBeforeEnd(
     submitBtn.parentNode,
-    `&nbsp;&nbsp;${simpleCheckboxHtml(prefAjaxifyDestroy)}`,
+    `&nbsp;&nbsp;${simpleCheckboxHtml(prefAjaxifyDestroy)}&nbsp;&nbsp;${
+      simpleCheckboxHtml(prefDisableDestroyPrompts)}`,
   );
-  on(submitBtn.parentNode, 'change', changePref);
-};
+  on(submitBtn.parentNode, 'change', changePref());
+}
 
 export default function interceptDestroy() {
-  injectPref();
+  injectPrefs();
+  initDestroyPrompt();
   ajaxifyDestroy = getValue(prefAjaxifyDestroy);
-  on(document.forms[0], 'submit', checkItems);
-  window.check = check;
+  on(document.forms[0], 'submit', submitHandler);
+  window.check = scopedCheck;
 }
