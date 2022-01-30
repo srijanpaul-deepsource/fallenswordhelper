d2f9bf6704c40d93b9cbba9d9bbe0f0885060bf1
Adrian Tompkins <atompkins@users.noreply.github.com>
2022-01-30 21:22:00 +0000

    fix highlightPvpProtection (#317)

--- src/modules/profile/highlightPvpProtection.js
+++ src/modules/profile/highlightPvpProtection.js
@@ -1,3 +1,4 @@
+import './highlightPvpProtection.css';
 import getTextTrim from '../common/getTextTrim';
 import { pointsUrl } from '../support/constants';
 import querySelector from '../common/querySelector';
@@ -5,6 +6,6 @@ import querySelector from '../common/querySelector';
 export default function highlightPvpProtection() {
   const pvpp = querySelector(`#profileLeftColumn a[href="${pointsUrl}"]`);
   if (pvpp && getTextTrim(pvpp.parentNode.nextSibling) !== 'N/A') { // Text Node
-    pvpp.parentNode.parentNode.style.cssText = 'border: 3px solid red'; // TODO
+    pvpp.parentNode.parentNode.classList.add('fshPvpWarn');
   }
 }
